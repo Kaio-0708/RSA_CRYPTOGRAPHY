1b86655590e7aaa3318c408a7c69be19f535f013
Kaio0708 <kaiovitor0707@gmail.com>
2025-10-07 01:39:46 -0300

    adição de conceitos matemáticos da teoria dos números

--- src/cryptography/number_theory.py
+++ src/cryptography/number_theory.py
@@ -1,4 +1,5 @@
-# Optei por implementar a Exponenciação Modular Rápida (a^b mod n) em vez de usar pow() da biblioteca.
+# Optei por implementar todas as funções manualmente em vez de importar de biblioteca. 
+
 def mod_exp(a, b, n):
     result = 1
     a = a % n
@@ -8,3 +9,30 @@ def mod_exp(a, b, n):
         a = (a * a ) % n
         b //= 2
     return result
+
+def gcd(a,b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+def bezout(a, b):
+    x0, y0 = 1, 0
+    x1, y1 = 0, 1
+
+    while b != 0:
+        q = a // b
+        a, b = b, a % b
+        x0, x1 = x1, x0 - q * x1
+        y0, y1 = y1, y0 - q * y1
+    
+    return a, x0, y0
+
+def mod_inv(a, n):
+    g, x, y = bezout(a, n)
+    if g != 1:
+        print(f"Inverso modular não existe para {a} mod {n}")
+    return x % n
+
+def phi_euller(p, q):
+    return (p-1) * (q -1)
+    
